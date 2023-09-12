5a570b653f1401388b99a2fe673eeca29f8c26fa
Oto Brglez <otobrglez@gmail.com>
2023-09-12 21:34:30 +0200

    Working on GH Actions.

---
+++ bin/trigger-gh-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -ex
+
+GH_ENDPOINT="https://api.github.com/repos/ogrodje/ogrodje-site/actions/workflows/build/dispatches"
+
+curl -D - -X POST \
+		-H "Accept: application/vnd.github+json" \
+		-H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+		$GH_ENDPOINT
+
