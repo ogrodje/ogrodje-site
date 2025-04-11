e4d21fa4bf3cc04336d86771c19f6def78bfac8b
Oto Brglez <otobrglez@gmail.com>
2025-04-11 14:51:51 +0200

    Adding events.

---
+++ src/services/EventsService.ts
@@ -0,0 +1,25 @@
+export interface Event {
+  id: string
+  meetupID: string
+  source: string
+  sourceURL: string
+  title: string
+  eventURL?: string
+  meetupName?: string
+  startDateTime: string
+  endDateTime: string
+  hasStartTime: boolean
+  hasEndTime: boolean
+  locationName?: string
+  locationAddress?: string
+  description?: string
+}
+
+export class EventsService {
+  static eventsEndpoint: string = "https://goo.ogrodje.si";
+
+  static async timeline(): Promise<Event[]> {
+    return fetch(`${this.eventsEndpoint}/timeline`).then((res) => res.json());
+  }
+
+}
