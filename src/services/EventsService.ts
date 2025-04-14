f0e7b8d03d11986b5bbfb09a38d75f53dcea0124
Oto Brglez <otobrglez@gmail.com>
2025-04-14 13:08:31 +0200

    Fixing date-time issues

--- src/services/EventsService.ts
+++ src/services/EventsService.ts
@@ -12,14 +12,38 @@ export interface Event {
   hasEndTime: boolean
   locationName?: string
   locationAddress?: string
-  description?: string
+  description?: string,
+  localStartDateTime: Date,
+  localEndDateTime?: Date,
+}
+
+const convertToCEST = (isoDatetime: string): Date => {
+  const utcDate = new Date(isoDatetime);
+
+  if (isNaN(utcDate.getTime())) {
+    throw new Error('Invalid ISO datetime format');
+  }
+
+  const utcOffsetMinutes = utcDate.getUTCMinutes() + (utcDate.getTimezoneOffset() + 120); // Add +120 for CEST (UTC+2)
+  const cestDate = new Date(utcDate);
+
+  cestDate.setMinutes(utcOffsetMinutes);
+
+  return cestDate;
 }
 
 export class EventsService {
   static eventsEndpoint: string = "https://goo.ogrodje.si";
 
   static async timeline(): Promise<Event[]> {
-    return fetch(`${this.eventsEndpoint}/timeline`).then((res) => res.json());
+    return fetch(`${this.eventsEndpoint}/timeline`)
+      .then((res) => res.json())
+      .then((json) => json.map((e: any) => ({
+        ...e,
+        localStartDateTime: convertToCEST(e.startDateTime),
+        localEndDateTime: e.endDateTime ? convertToCEST(e.endDateTime) : undefined
+      })))
+      ;
   }
 
 }
