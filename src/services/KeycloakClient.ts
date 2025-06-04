9309e157fd3c1103f527585a33d947f235896989
Oto Brglez <otobrglez@gmail.com>
2025-06-04 11:35:17 +0200

    what

--- src/services/KeycloakClient.ts
+++ src/services/KeycloakClient.ts
@@ -1,9 +1,16 @@
 import type {KeycloakConfig} from "keycloak-js";
 
-export const configForKeycloak: () => KeycloakConfig = () => ({
-    url: import.meta.env.KEYCLOAK_URL,
-    realm: import.meta.env.KEYCLOAK_REALM,
-    clientId: import.meta.env.KEYCLOAK_CLIENT_ID,
-  } as KeycloakConfig
-)
+export const configForKeycloak: () => KeycloakConfig = () => {
+
+
+    const config = {
+        url: import.meta.env.KEYCLOAK_URL,
+        realm: import.meta.env.KEYCLOAK_REALM,
+        clientId: import.meta.env.KEYCLOAK_CLIENT_ID,
+    } as KeycloakConfig;
+
+    console.log("Keycloak config: ", config);
+
+    return config;
+}
 
